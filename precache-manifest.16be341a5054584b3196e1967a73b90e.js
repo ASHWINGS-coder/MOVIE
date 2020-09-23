afb33a3945f82a73e67bc0a12d0129d576a1f816
Ash <ashwin.gs@rhatechnology.com>
2020-09-23 17:22:52 +0530

    Updates

---
+++ precache-manifest.16be341a5054584b3196e1967a73b90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104eb88146c1d5d163453e9778eac3d7",
+    "url": "/MOVIE/index.html"
+  },
+  {
+    "revision": "c1e712c1987cd285394e",
+    "url": "/MOVIE/static/css/main.e1b462dd.chunk.css"
+  },
+  {
+    "revision": "7f1803882a980d0d4957",
+    "url": "/MOVIE/static/js/2.8db9a878.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MOVIE/static/js/2.8db9a878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e712c1987cd285394e",
+    "url": "/MOVIE/static/js/main.b35cbdea.chunk.js"
+  },
+  {
+    "revision": "20e33fa32b0765c4b057",
+    "url": "/MOVIE/static/js/runtime-main.55890245.js"
+  }
+]);
